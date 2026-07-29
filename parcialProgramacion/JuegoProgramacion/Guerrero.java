d14f9294585a710191442f604561c68041b89a56
Luciano Bazzano <lucianobazzano@Lucianos-MacBook-Air.local>
2026-07-29 10:54:51 -0300

    PARCIAL

--- parcialProgramacion/JuegoProgramacion/Guerrero.java
+++ parcialProgramacion/JuegoProgramacion/Guerrero.java
@@ -76,9 +76,6 @@ y después inicia los atributos propios. */
     }
 
 //---------------------------- GETTERS Y SETTERS ----------------------------
-//Puertas de acceso
-//GET permite que otras clases vean y lean el valor de la variable
-//SET permite que otras clases cambien el valor
 
     public int getFuerza() {
         return fuerza;
